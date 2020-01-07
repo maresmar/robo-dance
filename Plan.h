88ee7647f9767bc6b69d8f7ad56c0df20b5dabd6
Jan Waltl <waltl.jan@gmail.com>
2020-01-07 16:36:59 +0100

    Implement planning

--- Plan.h
+++ Plan.h
@@ -14,15 +14,133 @@ enum PlanEntry : uint8_t {
   Wait,
   Finished,
 };
+enum class CurrDir : char {
+  EAST = 'E',
+  NORTH = 'N',
+  SOUTH = 'S',
+  WEST = 'W',
+};
+struct RobotConfig {
+  uint8_t column, row;
+  CurrDir curr_dir;
+};
 
+struct CompPlanEntry {
+  uint8_t column : 4;
+  uint8_t row : 4;
+  bool rowFirst : 1;
+  uint16_t timePoint : 15;
+};
+static CurrDir rotateLeft(CurrDir c) {
+  switch (c) {
+  case CurrDir::EAST:
+    return CurrDir::NORTH;
+  case CurrDir::NORTH:
+    return CurrDir::WEST;
+  case CurrDir::WEST:
+    return CurrDir::SOUTH;
+  case CurrDir::SOUTH:
+    return CurrDir::EAST;
+  }
+}
+static CurrDir rotateRight(CurrDir c) {
+  switch (c) {
+  case CurrDir::EAST:
+    return CurrDir::SOUTH;
+  case CurrDir::NORTH:
+    return CurrDir::EAST;
+  case CurrDir::WEST:
+    return CurrDir::NORTH;
+  case CurrDir::SOUTH:
+    return CurrDir::WEST;
+  }
+}
+// An interface that executes the currently active plan stored in planner.
 class Plan {
 public:
+  Plan(uint8_t num_entries, CompPlanEntry *entries, RobotConfig init_pos)
+      : entries(entries), num_entries(num_entries), init_pos(init_pos),
+        curr_pos(init_pos), curr_entry_i(0) {}
+
   // Returns next command that should be executed by the robot given the time
   // elapsed from the beginning.
-  PlanEntry getNext(unsigned long miliTimeElapsed) {}
+  PlanEntry getNext(unsigned long miliTimeElapsed) {
+    uint16_t deciTimeElapsed = miliTimeElapsed / 100;
+    // In case of an empty plan
+    if (curr_entry_i >= num_entries)
+      return PlanEntry::Finished;
+
+    CompPlanEntry &curr_cmd = entries[curr_entry_i];
+    CurrDir c_dir = curr_pos.curr_dir;
+    CurrDir d_dir = desired_dir();
+
+    // Too early
+    if (curr_cmd.timePoint > deciTimeElapsed)
+      return PlanEntry::Wait;
+
+    // Orientation incorrect
+    if (d_dir != c_dir) {
+      if (rotateRight(c_dir) == d_dir) {
+        curr_pos.curr_dir = d_dir;
+        return PlanEntry::Right;
+      } else {
+        curr_pos.curr_dir = rotateLeft(c_dir);
+        return PlanEntry::Left;
+      }
+    }
+    // Go straight
+    if (curr_pos.row != curr_cmd.row || curr_pos.column != curr_cmd.column) {
+      switch (c_dir) {
+      case CurrDir::NORTH:
+        curr_pos.row += 1;
+        break;
+      case CurrDir::SOUTH:
+        curr_pos.row -= 1;
+        break;
+      case CurrDir::EAST:
+        curr_pos.column += 1;
+        break;
+      case CurrDir::WEST:
+        curr_pos.column -= 1;
+        break;
+      }
+      return PlanEntry::Go;
+    }
+
+    // The command is already finished, fetch a new one.
+    ++curr_entry_i;
+    // Should be only one level-deep recursion since the next command is not
+    // immediately completed.
+    return getNext(miliTimeElapsed);
+  }
   // Reset the plan to the first instruction
-  void reset() {}
+  // ENSURE that the robot is located at its initial position as specified
+  // by the plan.
+  void reset() { curr_entry_i = 0; }
   // Get the plan that will return the robot to the initial position
   Plan getReverse();
+
+private:
+  CurrDir desired_dir() {
+    CompPlanEntry &curr_cmd = entries[curr_entry_i];
+    int col_delta = (int)curr_cmd.column - curr_pos.column;
+    int row_delta = (int)curr_cmd.row - curr_pos.row;
+    if (col_delta == row_delta && col_delta == 0)
+      return curr_pos.curr_dir;
+
+    CurrDir row_orient = row_delta > 0 ? CurrDir::NORTH : CurrDir::SOUTH;
+    CurrDir col_orient = col_delta > 0 ? CurrDir::EAST : CurrDir::WEST;
+    if (curr_cmd.rowFirst)
+      return row_delta != 0 ? row_orient : col_orient;
+    else
+      return col_delta != 0 ? col_orient : row_orient;
+  }
+
+  CompPlanEntry *entries;
+  uint8_t num_entries;
+  RobotConfig init_pos;
+  RobotConfig curr_pos;
+  uint8_t curr_entry_i;
 };
+
 #endif
---
+++ Planner.h
@@ -0,0 +1,254 @@
+#ifndef PLAN_PARSER_HEADER
+#define PLAN_PARSER_HEADER
+// Only include one please, because of the static variables that should really
+// be in .cpp files
+#include <cassert>
+#include <cctype>
+#include <cstdint>
+#include <cstdlib>
+#include <stdio.h>
+
+#include "Plan.h"
+
+#ifndef RUNNING_TESTS
+//#include <Arduino.h>
+#define PL_PRINT_PARSE_ERR_STR(x) (Serial.println(x))
+#else
+#define F(x) x
+#define PL_PRINT_PARSE_ERR_STR(x) printf("ERR: %s\n", (x));
+#endif
+
+enum class ParseError {
+  OK,
+  InvalidOrient,
+  InvalidCoords,
+  InvalidTime,
+  UnexpectedEnd,
+  PlanTooLong
+};
+
+struct StringGetter {
+  StringGetter(const char *str) : str(str) {}
+  // Will return '\0' on error and keep returning it
+  char getNext(bool skip_whitespace = true) {
+    char c = peek_next(skip_whitespace);
+    if (c != '\0')
+      ++str;
+    return c;
+  }
+  void skip_whitespace() {
+    while (str && std::isspace(*str))
+      ++str;
+  }
+  char peek_next(bool skip_whitespace = true) {
+    if (skip_whitespace)
+      this->skip_whitespace();
+    char c = str ? *str : '\0';
+    return c;
+  }
+  const char *str;
+};
+
+// Parses two-letter string into coordinates.
+static ParseError parseCoords(const char str[2], bool &col_first, uint8_t &row,
+                              uint8_t &col);
+// Parsers string of len 3: 'XYZ', X\in{1..9}, Y\in{A...I}, Z==CurrDir
+static ParseError parseCommand(const char str[2 + 5 + 1],
+                               CompPlanEntry &newEntry);
+// Parse null terminated string of length max 8, str[0,1]==Coords,
+// str[2+]==timepoint integer.
+static ParseError parseInitPos(const char *str, RobotConfig &initConfig);
+static ParseError preparseCmd(StringGetter &getter,
+                              char command_str[2 + 5 + 1]) {
+  command_str[0] = getter.getNext();
+  command_str[1] = getter.getNext();
+  char t = std::tolower(getter.getNext());
+  if (t == '\0')
+    return ParseError::UnexpectedEnd;
+  if (t != 't')
+    return ParseError::InvalidCoords;
+
+  // Read the time, only skip the first whitespace
+  //  = between 'T' and the first digit
+  // Time is missing
+  if (getter.peek_next() == '\0')
+    return ParseError::UnexpectedEnd;
+
+  for (int i = 0; i < 6; ++i) {
+    char last = command_str[2 + i] = getter.getNext(false);
+    // The number ended
+    if (last == '\0' || std::isspace(last)) {
+      command_str[2 + i] = '\0';
+      break;
+    } else if (i == 5) // Time won't fit into 16bits
+      return ParseError::InvalidTime;
+  }
+  return ParseError::OK;
+}
+
+// Consumes 3B*64 * 2 RAM = 384B, 768B EEPROM
+constexpr const static uint8_t MaxPlanLength = 64;
+constexpr const static uint8_t NumEEPROMSlots = 4;
+
+// Responsible for loading and saving plans. A plan can be loaded from a string
+// or EEPROM.
+class Planner {
+public:
+  static bool loadDefault() {
+    const char *str = "B1N E1T150 b2T350 3At450 4CT567 D2T700";
+    return loadFromString(str);
+  }
+  static bool loadFromString(const char *str) {
+    StringGetter getter = StringGetter(str);
+    ParseError err = load(getter);
+    if (err != ParseError::OK)
+      printErrMsg(err);
+    return err == ParseError::OK;
+  }
+  static void loadFromSerial();
+  static void loadFromEEPROM(int slot);
+  static void saveToEEPROM(int slot);
+
+  // The returned PLan is valid until next loadXXX method is called.
+  static Plan getActivePlan() {
+    return Plan(num_active_entries, active_entries, active_init_pos);
+  }
+
+private:
+  Planner() {}
+  static void printErrMsg(ParseError e) {
+    switch (e) {
+    case ParseError::OK:
+      PL_PRINT_PARSE_ERR_STR(F("No errors."));
+      break;
+    case ParseError::InvalidOrient:
+      PL_PRINT_PARSE_ERR_STR(F("Invalid orientation in the initial position."));
+      break;
+    case ParseError::InvalidCoords:
+      PL_PRINT_PARSE_ERR_STR(F("Invalid coordinates."));
+      break;
+    case ParseError::InvalidTime:
+      PL_PRINT_PARSE_ERR_STR(F("Invalid time."));
+      break;
+    case ParseError::PlanTooLong:
+      PL_PRINT_PARSE_ERR_STR(F("Too many commands."));
+      break;
+    case ParseError::UnexpectedEnd:
+      PL_PRINT_PARSE_ERR_STR(F("Unexpected end of input."));
+      break;
+    };
+  }
+  static ParseError load(StringGetter &getter) {
+    char init_pos[3];
+    init_pos[0] = getter.getNext();
+    init_pos[1] = getter.getNext();
+    init_pos[2] = getter.getNext();
+    if (init_pos[2] == '\0')
+      return ParseError::UnexpectedEnd;
+
+    RobotConfig new_init;
+    ParseError err = parseInitPos(init_pos, new_init);
+    if (err != ParseError::OK)
+      return err;
+
+    char new_cmd_str[2 + 5 + 1];
+    // Parse commands
+    int n = 0;
+    for (; n < MaxPlanLength; ++n) {
+      // Input ended
+      if (getter.peek_next() == '\0') {
+        break;
+      }
+
+      err = preparseCmd(getter, new_cmd_str);
+      if (err != ParseError::OK)
+        return err;
+      err = parseCommand(new_cmd_str, loaded_entries[n]);
+      if (err != ParseError::OK)
+        return err;
+    }
+    if (getter.getNext() != '\0')
+      return ParseError::PlanTooLong;
+
+    // Plan loaded successfully -> set as active
+
+    for (int i = 0; i < n; ++i)
+      active_entries[i] = loaded_entries[i];
+    num_active_entries = n;
+    active_init_pos = new_init;
+    return ParseError::OK;
+  }
+
+  // Skips any whitespace symbols
+  static void skip_whitespace(const char *&str) {
+    while (str && std::isspace(*str))
+      ++str;
+  }
+// For easier testing, do not  misuse!
+#ifdef RUNNING_TESTS
+public:
+#endif
+  // Currently active plan loaded into the memory
+  static CompPlanEntry active_entries[MaxPlanLength];
+  // Scratch space for loading a new plan
+  // Seems wastefull but it ensures that if the plan fails in the middle of
+  // loading then the original plan is preserved.
+  static CompPlanEntry loaded_entries[MaxPlanLength];
+  static int num_active_entries;
+  static RobotConfig active_init_pos;
+};
+
+int Planner::num_active_entries = 0;
+CompPlanEntry Planner::loaded_entries[MaxPlanLength];
+CompPlanEntry Planner::active_entries[MaxPlanLength];
+RobotConfig Planner::active_init_pos;
+
+static ParseError parseCoords(const char str[2], bool &col_first, uint8_t &row,
+                              uint8_t &col) {
+  col_first = std::isalpha(str[0]);
+  uint8_t col_index = col_first ? 0 : 1;
+  uint8_t row_index = 1 - col_index;
+  col = std::tolower(str[col_index]) - 'a' + 1;
+  row = str[row_index] - '0';
+
+  return (col < 1 || col > 9 || row < 1 || row > 9) ? ParseError::InvalidCoords
+                                                    : ParseError::OK;
+}
+
+static ParseError parseInitPos(const char *str, RobotConfig &initConfig) {
+  bool col_first;
+  uint8_t row, col;
+
+  ParseError coords_err = parseCoords(str, col_first, row, col);
+  if (coords_err != ParseError::OK)
+    return coords_err;
+  CurrDir init_dir = (CurrDir)std::toupper(str[2]);
+
+  if (init_dir != CurrDir::NORTH && init_dir != CurrDir::SOUTH &&
+      init_dir != CurrDir::EAST && init_dir != CurrDir::WEST)
+    return ParseError::InvalidOrient;
+
+  initConfig.column = col;
+  initConfig.row = row;
+  initConfig.curr_dir = init_dir;
+  return ParseError::OK;
+}
+static ParseError parseCommand(const char str[2 + 5 + 1],
+                               CompPlanEntry &newEntry) {
+  bool col_first;
+  uint8_t row, col;
+  ParseError coords_err = parseCoords(str, col_first, row, col);
+  if (coords_err != ParseError::OK)
+    return coords_err;
+
+  int timePoint = std::atoi(str + 2);
+  if (timePoint == 0)
+    return ParseError::InvalidTime;
+
+  newEntry.column = col & 0x0F;
+  newEntry.row = row & 0x0F;
+  newEntry.rowFirst = (!col_first) % 2;
+  newEntry.timePoint = timePoint & 0x7FFF;
+  return ParseError::OK;
+}
+#endif
